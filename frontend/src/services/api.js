58cb4f86c05767aca602c404e1fb77989a97269c
Tanya Abrol <tanyaabrol12@gmail.com>
2025-08-26 11:14:23 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://college-grievance-portal.onrender.com',
+  baseURL: 'https://college-grievance-portal.onrender.com/api',
   timeout: 10000,
 });
 
